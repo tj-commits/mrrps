31f36d3c51355d6d4d4df1436bb045b85495f0af
tj-commits <thefoxjones@gmail.com>
2024-07-27 10:24:34 -0500

    commit

--- src/pages/log-out.tsx
+++ src/pages/log-out.tsx
@@ -1,13 +1,18 @@
 import { NextPage } from "next";
-import { signOut } from "next-auth/react";
+import { signOut, useSession } from "next-auth/react";
 import { Button } from "~/components/Button";
 
 const LogOut: NextPage = () => {
+
+  const session = useSession()
+
+  if (session.status !== 'authenticated') location.replace('/')
+
   return <>
-    <div className="px-5 py-5">
+    {session.status === 'authenticated' && (<div className="px-5 py-5">
       <p className="text-2xl">Are you sure that you want to sign out?</p><br />
       <Button onClick={() => signOut()}>Yes</Button>
-    </div>
+    </div>)}
   </>
 }
 
