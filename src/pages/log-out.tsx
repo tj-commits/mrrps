793cf0e9b86e0ce480feddc5885239696c2e0d35
tj-commits <thefoxjones@gmail.com>
2024-07-27 10:21:26 -0500

    commit

--- src/pages/log-out.tsx
+++ src/pages/log-out.tsx
@@ -1,8 +1,13 @@
 import { NextPage } from "next";
+import { signOut } from "next-auth/react";
+import { Button } from "~/components/Button";
 
 const LogOut: NextPage = () => {
   return <>
-    <h1>Log out</h1>
+    <div className="px-5 py-5">
+      <p className="text-2xl">Are you sure that you want to sign out?</p><br />
+      <Button onClick={() => signOut()}>Yes</Button>
+    </div>
   </>
 }
 
